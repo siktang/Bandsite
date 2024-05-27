277a156847da8023f8020e11619c749a144bad57
Denise Deng <siktang1221@gmail.com>
2024-05-27 16:51:45 -0400

    tested getShows() - worked

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -3,7 +3,7 @@ const API_KEY = "c33cf7a7-78ec-4150-ac97-2f7c69867d88";
 
 
 // Preset functions
-const createComments = (comments) => {
+const displayComments = (comments) => {
     const commentSubmitted = document.createElement("section");
     commentSubmitted.classList.add("comment__submitted");
     commentBox.appendChild(commentSubmitted);
@@ -31,8 +31,8 @@ const createComments = (comments) => {
 
     const commentDate = document.createElement("h3");
     commentDate.classList.add("comment__submitted--date");
-    const commentDateDetails = new Date(comments.timestamp);
-    commentDate.textContent = commentDateDetails.toLocaleDateString("es-pa");
+    const commentDateTimestamp = new Date(comments.timestamp);
+    commentDate.textContent = commentDateTimestamp.toLocaleDateString("es-pa");
     commentHeading.appendChild(commentDate);
 
     const commentDetails = document.createElement("p");
@@ -44,6 +44,54 @@ const createComments = (comments) => {
     divider.classList.add("divider");
     commentSubmitted.appendChild(divider);
 }
+
+const displayShows = (showItem) => {
+    const showItems = document.createElement("div");
+    showItems.classList.add("shows__show-item");
+    showsContainer.appendChild(showItems);
+
+    const showDateTitle = document.createElement("h4");
+    showDateTitle.classList.add("shows__show-item--mobile-header");
+    showDateTitle.textContent= "DATE";
+    showItems.appendChild(showDateTitle);
+
+    const showDate = document.createElement("p");
+    showDate.classList.add("shows__show-item--date");
+    const showDateTimestamp = new Date(showItem.date);
+    showDate.textContent= showDateTimestamp.toDateString();
+    showItems.appendChild(showDate);
+
+    const showVenueTitle = document.createElement("h4");
+    showVenueTitle.classList.add("shows__show-item--mobile-header");
+    showVenueTitle.textContent= "VENUE";
+    showItems.appendChild(showVenueTitle);
+
+    const showVenue = document.createElement("p");
+    showVenue.classList.add("shows__show-item--place");
+    showVenue.textContent= showItem.place;
+    showItems.appendChild(showVenue);
+
+    const showLocationTitle = document.createElement("h4");
+    showLocationTitle.classList.add("shows__show-item--mobile-header");
+    showLocationTitle.textContent= "LOCATION";
+    showItems.appendChild(showLocationTitle);
+
+    const showLocation = document.createElement("p");
+    showLocation.classList.add("shows__show-item--place");
+    showLocation.textContent= showItem.location;
+    showItems.appendChild(showLocation);
+
+    const button = document.createElement("button")
+    button.classList.add("shows__show-item--button");
+    button.textContent = "BUY TICKETS";
+    showItems.appendChild(button);
+
+    const divider = document.createElement("div");
+    divider.classList.add("divider--mobile");
+    showsContainer.appendChild(divider);
+}
+
+// Class created
 
 class BandSiteApi {
     constructor(apiKey) {
@@ -60,12 +108,14 @@ class BandSiteApi {
 
         const createComment = async() => {
             try {
-                const newComment = await axios.post(
-                    `${this.baseUrl}comments?api_key=${this.Key}`, 
-                    {name: commentObject.value, 
+                const newComment = {name: commentObject.value, 
                     date: new Date(), 
                     comment: commentObject.comment.value}
-                )
+
+                await axios.post(
+                    `${this.baseUrl}comments?api_key=${this.Key}`, 
+                    newComment
+                );
                 // Axios will turn the object into JSON automatically
             } catch (error) {
                 console.log(error);
@@ -88,7 +138,7 @@ class BandSiteApi {
                 } );
 
                 console.log(commentsList);
-                commentsList.forEach((comment) => createComments(comment));
+                commentsList.forEach((comment) => displayComments(comment));
                 
 
             } catch (error) {
@@ -105,6 +155,7 @@ class BandSiteApi {
                 // Axios already parsed JSON data when fetching
                 const showsList = showsData.data; 
 
+                showsList.forEach((showItem) => displayShows(showItem));
                 console.log(showsList);
             } catch (error) {
                 console.log(error);
@@ -122,6 +173,7 @@ class BandSiteApi {
 // test();
 
 const bioAPI = new BandSiteApi(API_KEY);
-
 bioAPI.getComment();
-bioAPI.getShows();
+
+const showsAPI = new BandSiteApi(API_KEY);
+showsAPI.getShows();
--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,85 +1,5 @@
-// Preset functions
-
-const displayShows = (showItem) => {
-    const showItems = document.createElement("div");
-    showItems.classList.add("shows__show-item");
-    showsContainer.appendChild(showItems);
-
-    const showDateTitle = document.createElement("h4");
-    showDateTitle.classList.add("shows__show-item--mobile-header");
-    showDateTitle.textContent= "DATE";
-    showItems.appendChild(showDateTitle);
-
-    const showDate = document.createElement("p");
-    showDate.classList.add("shows__show-item--date");
-    showDate.textContent= showItem.date.toDateString();
-    showItems.appendChild(showDate);
-
-    const showVenueTitle = document.createElement("h4");
-    showVenueTitle.classList.add("shows__show-item--mobile-header");
-    showVenueTitle.textContent= "VENUE";
-    showItems.appendChild(showVenueTitle);
-
-    const showVenue = document.createElement("p");
-    showVenue.classList.add("shows__show-item--place");
-    showVenue.textContent= showItem.venue;
-    showItems.appendChild(showVenue);
-
-    const showLocationTitle = document.createElement("h4");
-    showLocationTitle.classList.add("shows__show-item--mobile-header");
-    showLocationTitle.textContent= "LOCATION";
-    showItems.appendChild(showLocationTitle);
-
-    const showLocation = document.createElement("p");
-    showLocation.classList.add("shows__show-item--place");
-    showLocation.textContent= showItem.location;
-    showItems.appendChild(showLocation);
-
-    const button = document.createElement("button")
-    button.classList.add("shows__show-item--button");
-    button.textContent = "BUY TICKETS";
-    showItems.appendChild(button);
-
-    const divider = document.createElement("div");
-    divider.classList.add("divider--mobile");
-    showsContainer.appendChild(divider);
-}
 
 
-// Arrays to be used
-
-const showList = [
-    {
-        date: new Date("Mon Sept 09 2024"), 
-        venue:"Ronald Lane", 
-        location:"San Francisco, CA"
-    }, 
-    {
-        date: new Date("Tue Sept 17 2024"), 
-        venue:"Ronald Lane", 
-        location:"San Francisco, CA"
-    },
-    {
-        date: new Date("Sat Oct 12 2024"), 
-        venue:"View Lounge", 
-        location:"San Francisco, CA"
-    },
-    {
-        date: new Date("Sat Nov 16 2024"), 
-        venue:"Hyatt Agency", 
-        location:"San Francisco, CA"
-    },
-    {
-        date: new Date("Fri Nov 29 2024"), 
-        venue:"Moscow Center", 
-        location:"San Francisco, CA"
-    },
-    {
-        date: new Date("Wed Dec 18 2024"), 
-        venue:"Press Club", 
-        location:"San Francisco, CA"
-    },
-];
 
 
 // Constructing show section
@@ -109,7 +29,7 @@ tabletLocationTitle.classList.add("shows__tablet--header--location");
 tabletLocationTitle.textContent = "LOCATION";
 tabletHeaderContainer.appendChild(tabletLocationTitle);
 
-showList.forEach((item) => displayShows(item));
+// showList.forEach((item) => displayShows(item));
 
 
 // Add event listener for showitems' onclick
@@ -125,3 +45,88 @@ showItems.forEach((showItem) => {
         showItem.classList.add("shows__selected");
     })
 });
+
+
+
+/***********
+ * To be deleted before submission
+ * ************/ 
+// const displayShows = (showItem) => {
+//     const showItems = document.createElement("div");
+//     showItems.classList.add("shows__show-item");
+//     showsContainer.appendChild(showItems);
+
+//     const showDateTitle = document.createElement("h4");
+//     showDateTitle.classList.add("shows__show-item--mobile-header");
+//     showDateTitle.textContent= "DATE";
+//     showItems.appendChild(showDateTitle);
+
+//     const showDate = document.createElement("p");
+//     showDate.classList.add("shows__show-item--date");
+//     showDate.textContent= showItem.date.toDateString();
+//     showItems.appendChild(showDate);
+
+//     const showVenueTitle = document.createElement("h4");
+//     showVenueTitle.classList.add("shows__show-item--mobile-header");
+//     showVenueTitle.textContent= "VENUE";
+//     showItems.appendChild(showVenueTitle);
+
+//     const showVenue = document.createElement("p");
+//     showVenue.classList.add("shows__show-item--place");
+//     showVenue.textContent= showItem.venue;
+//     showItems.appendChild(showVenue);
+
+//     const showLocationTitle = document.createElement("h4");
+//     showLocationTitle.classList.add("shows__show-item--mobile-header");
+//     showLocationTitle.textContent= "LOCATION";
+//     showItems.appendChild(showLocationTitle);
+
+//     const showLocation = document.createElement("p");
+//     showLocation.classList.add("shows__show-item--place");
+//     showLocation.textContent= showItem.location;
+//     showItems.appendChild(showLocation);
+
+//     const button = document.createElement("button")
+//     button.classList.add("shows__show-item--button");
+//     button.textContent = "BUY TICKETS";
+//     showItems.appendChild(button);
+
+//     const divider = document.createElement("div");
+//     divider.classList.add("divider--mobile");
+//     showsContainer.appendChild(divider);
+// }
+
+// // Arrays to be used
+
+// const showList = [
+//     {
+//         date: new Date("Mon Sept 09 2024"), 
+//         venue:"Ronald Lane", 
+//         location:"San Francisco, CA"
+//     }, 
+//     {
+//         date: new Date("Tue Sept 17 2024"), 
+//         venue:"Ronald Lane", 
+//         location:"San Francisco, CA"
+//     },
+//     {
+//         date: new Date("Sat Oct 12 2024"), 
+//         venue:"View Lounge", 
+//         location:"San Francisco, CA"
+//     },
+//     {
+//         date: new Date("Sat Nov 16 2024"), 
+//         venue:"Hyatt Agency", 
+//         location:"San Francisco, CA"
+//     },
+//     {
+//         date: new Date("Fri Nov 29 2024"), 
+//         venue:"Moscow Center", 
+//         location:"San Francisco, CA"
+//     },
+//     {
+//         date: new Date("Wed Dec 18 2024"), 
+//         venue:"Press Club", 
+//         location:"San Francisco, CA"
+//     },
+// ];
